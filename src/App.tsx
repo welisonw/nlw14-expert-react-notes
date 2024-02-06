30c0b3bce37b9dc2a41ede37b292de6b97d8e932
Welison Wessling <wwelisonw@gmail.com>
2024-02-06 16:15:56 -0300

    feat(App.tsx): added NewNoteCard and NoteCard to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import Logo from './assets/logo.svg';
 
+import { NewNoteCard } from './components/NewNoteCard';
+import { NoteCard } from './components/NoteCard';
+
 export function App() {
 	return (
 		<div className='max-w-6xl mx-auto my-12 space-y-6'>
@@ -14,6 +17,15 @@ export function App() {
 			</form>
 
 			<div className='h-px bg-slate-700' />
+
+			<div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+				<NewNoteCard />
+
+				<NoteCard />
+				<NoteCard />
+				<NoteCard />
+				<NoteCard />
+			</div>
 		</div>
 	);
 }
