5300a07a8859c4bb3d35bbb5a214b51e6e23fe0c
Welison Wessling <wwelisonw@gmail.com>
2024-02-06 15:24:02 -0300

    feat: created header with logo and note search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import Logo from './assets/logo.svg';
+
 export function App() {
-	return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+	return (
+		<div className='max-w-6xl mx-auto my-12 space-y-6'>
+			<img src={Logo} alt='logo nlw expert' />
+
+			<form action='' className='w-full'>
+				<input
+					type='text'
+					placeholder='Busque em suas notas...'
+					className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+				/>
+			</form>
+
+			<div className='h-px bg-slate-700' />
+		</div>
+	);
 }
